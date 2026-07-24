6f07019e6fbbecad11bd722de6b5b7f0ec9cf145
cesarcalvo171-cyber <cesarcalvo171@gmail.com>
2026-07-24 15:52:32 -0600

    Update Supabase credentials

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://TU_NUEVO_PROYECTO.supabase.co';
-const supabaseAnonKey = 'TU_NUEVA_CLAVE_ANON';
+const supabaseUrl = 'https://jtwauicxroxtgseqdhpn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0d2F1aWN4cm94dGdzZXFkaHBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MjU3NjAsImV4cCI6MjEwMDUwMTc2MH0.T6JWMrTpcy5ljfq9ZFKnAV3vaFZcOqoOIsKlADEvczo';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
